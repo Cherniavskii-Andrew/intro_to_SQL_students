70108f1e43090021160d163c19fa6ff7889835d5
Андрей Чернявский <corrosiveflake@gmail.com>
2026-01-30 22:03:03 +0300

    Решение заданий

--- task3/solution.sql
+++ task3/solution.sql
@@ -4,5 +4,5 @@ update server_logs set Session_Dur=24*60*(julianday(Session_End)-julianday(Sessi
 create view v_users_activity as
 select users.User_ID as User_ID, users.First_Name as First_Name, users.Last_Name as Last_Name, count(Log_ID) as Num_Sessions,sum(Session_Dur) as Total_Session_Time from users 
 left join server_logs on users.User_ID=server_logs.User_ID
-group by User_ID
+group by users.User_ID
 order by Total_Session_Time desc
