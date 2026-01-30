7f8da8d9b144e197b60400b731ad4e08d25722e3
Андрей Чернявский <corrosiveflake@gmail.com>
2026-01-30 20:45:01 +0300

    Решение заданий

--- task1/solution.sql
+++ task1/solution.sql
@@ -1,3 +1,3 @@
 create view v_users_age as 
-  select first_name, last_name, age from users where age<=30 and age>=20 order by users.age
+  select first_name, last_name, age from users where age<=30 and age>=19 order by users.age
 
